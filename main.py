9b2416f233f9d4d8df6370d3ea99e19dbf2c7232
Babar Baig <BobBaig@outlook.com>
2023-05-22 05:24:09 +0000

    Continue debugging

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
+""" Not sure what this does """
 import requests
 
+# Set your personal access token and username
+TOKEN = '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'
+USERNAME = 'BabarBaig'
+
 def github_repo_delete_bulk():
-    # Set your personal access token and username
-    TOKEN = 'YOUR_PERSONAL_ACCESS_TOKEN'
-    USERNAME = 'YOUR_GITHUB_USERNAME'
+    """ bulk delete github repos.  Prompt before each delete """
 
     # Fetch a list of repositories
+    url = f"https://api.github.com/users/{USERNAME}/repos"
     headers = {'Authorization': f'token {TOKEN}'}
-    response = requests.get(f'https://api.github.com/user/repos', headers=headers)
+    response = requests.get(url, headers=headers, timeout=20)
 
     # Check if the request was successful
-    if response.status_code == 200:
-        repositories = response.json()
+    if response.status_code != 200:
+        print(f"Failed to fetch repositories. Error: {response.status_code}: {response.text}")
+        return
 
-        # Filter the list of repositories based on your criteria
-        repositories_to_delete = [repo for repo in repositories if 'filter_condition' in repo['name']]
+    repositories = response.json()
 
-        # Delete the repositories
-        for repo in repositories_to_delete:
-            repo_name = repo['name']
-            delete_url = f'https://api.github.com/repos/{USERNAME}/{repo_name}'
-            delete_response = requests.delete(delete_url, headers=headers)
+    # Filter the list of repositories based on your criteria
+    repositories_to_delete = [repo for repo in repositories if 'aa' in repo['name']]
 
-            if delete_response.status_code == 204:
-                print(f"Repository '{repo_name}' deleted successfully.")
-            else:
-                print(f"Failed to delete repository '{repo_name}'. Error: {delete_response.text}")
+    # Delete the repositories
+    for repo in repositories_to_delete:
+        repo_name = repo['name']
+        delete_url = f'https://api.github.com/repos/{USERNAME}/{repo_name}'
+        question = f"Delete [{delete_url} ?]"
+        resp = input(question)
+        print(f'resp: {resp}')
+        if resp == 'q':
+            print('Goodbye!')
+            break
+        if resp == 'n':
+            continue
+        delete_response = requests.delete(delete_url, headers=headers, timeout=10)
 
-    else:
-        print(f"Failed to fetch repositories. Error: {response.text}")
+        if delete_response.status_code == 204:
+            print(f"Repository '{repo_name}' deleted successfully.")
+        else:
+            print(f"Failed to delete repository '{repo_name}'. Error: {delete_response.text}")
 
-print('Hello, world!')
 github_repo_delete_bulk()
